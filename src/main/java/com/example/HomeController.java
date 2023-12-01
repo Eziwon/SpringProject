7f3ed30d985863d1f76fc0396c20fc45cfa2e940
Eziwon <eziwon0228@gmail.com>
2023-12-01 21:54:59 +0900

    serverTime 구현

--- src/main/java/com/example/HomeController.java
+++ src/main/java/com/example/HomeController.java
@@ -1,2 +1,23 @@
-package com.example;public class HomeController {
+package com.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String home(Locale locale, Model model){
+        Date date = new Date();
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+
+        String formattedDate = dateFormat.format(date);
+        model.addAttribute("serverTime", formattedDate);
+
+        return "index";
+    }
 }
